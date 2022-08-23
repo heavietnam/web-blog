4769e3e2fb25e22e6ee6576f666f00964377f4cc
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2022-08-24 00:09:26 +0700

    create web blog

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,181 @@
+import { viteBundler } from "@vuepress/bundler-vite";
+import { webpackBundler } from "@vuepress/bundler-webpack";
+import { docsearchPlugin } from "@vuepress/plugin-docsearch";
+import { defineUserConfig } from "vuepress";
+import { gungnirTheme, i18n } from "vuepress-theme-gungnir";
+import { navbar, sidebar } from "./configs";
+
+const isProd = process.env.NODE_ENV === "production";
+
+export default defineUserConfig({
+  base: "/",
+  head: [
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: `/img/logo/favicon-16x16.png`
+      }
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: `/img/logo/favicon-32x32.png`
+      }
+    ],
+    ["link", { rel: "manifest", href: "/manifest.webmanifest" }],
+    ["meta", { name: "application-name", content: "Gungnir Theme" }],
+    ["meta", { name: "apple-mobile-web-app-title", content: "Gungnir Theme" }],
+    [
+      "meta",
+      { name: "apple-mobile-web-app-status-bar-style", content: "black" }
+    ],
+    [
+      "link",
+      { rel: "apple-touch-icon", href: `/img/logo/apple-touch-icon.png` }
+    ],
+    ["meta", { name: "theme-color", content: "#377bb5" }],
+    ["meta", { name: "msapplication-TileColor", content: "#377bb5" }]
+  ],
+
+  // site-level locales config
+  locales: {
+    "/": {
+      lang: "en-US",
+      title: "VuePress Theme Gungnir",
+      description: "A blog theme for VuePress"
+    },
+  },
+
+  // specify bundler via environment variable
+  bundler:
+    process.env.DOCS_BUNDLER === "webpack" ? webpackBundler() : viteBundler(),
+
+  // configure default theme
+  theme: gungnirTheme({
+    repo: "heavietnam/blog",
+    docsDir: "docs",
+
+    hitokoto: "https://v1.hitokoto.cn?c=i", // enable hitokoto (一言) or not?
+
+    // personal information
+    personalInfo: {
+      name: "Heavietnam",
+      avatar: "/img/avatar.jpeg",
+      description: "Chào mừng đến với heavietnam",
+      sns: {
+        github: "heavietnam",
+        linkedin: "xiaohan-zou-55bba0160",
+        facebook: "heavietnam",
+        twitter: "renovamen_zxh",
+        email: "renovamenzxh@gmail.com",
+      }
+    },
+
+    // header images on home page
+    homeHeaderImages: [
+      {
+        path: "/img/home-bg/1.jpg",
+        mask: "rgba(40, 57, 101, .4)"
+      },
+      {
+        path: "/img/home-bg/2.jpg",
+        mask: "rgb(251, 170, 152, .2)"
+      },
+      {
+        path: "/img/home-bg/3.jpg",
+        mask: "rgba(68, 74, 83, .1)"
+      },
+      {
+        path: "/img/home-bg/4.jpg",
+        mask: "rgba(19, 75, 50, .2)"
+      }
+    ],
+
+    // other pages
+    pages: {
+      tags: {
+        subtitle: "Black Sheep Wall",
+        bgImage: {
+          path: "/img/pages/tags.jpg",
+          mask: "rgba(211, 136, 37, .5)"
+        }
+      },
+      links: {
+        subtitle:
+          "When you are looking at the stars, please put the brightest star shining night sky as my soul.",
+        bgImage: {
+          path: "/img/pages/links.jpg",
+          mask: "rgba(64, 118, 190, 0.5)"
+        }
+      }
+    },
+
+    // theme-level locales config
+    locales: {
+      /**
+       * English locale config
+       *
+       * As the default locale is English, we don't need to set all of the locale fields
+       */
+      "/": {
+        // navbar
+        navbar: navbar.vi,
+        // sidebar
+        sidebar: sidebar.vi
+      },
+    },
+
+    themePlugins: {
+      // only enable git plugin in production mode
+      git: isProd,
+      katex: true,
+      mermaid: true,
+      chartjs: true,
+      giscus: {
+        repo: "This-is-an-Apple/gitalk-comments",
+        repoId: "MDEwOlJlcG9zaXRvcnkyMTA1MjQyMTQ=",
+        category: "Announcements",
+        categoryId: "DIC_kwDODIxYNs4CAO1u",
+        lazyLoad: true
+      },
+      mdPlus: {
+        all: true
+      },
+      ga: "G-EE8M2S3MPB",
+      ba: "10b7bc420625758a319d6b23aed4700f",
+      rss: {
+        siteURL: "https://v2.vuepress-theme-gungnir.vercel.app",
+        copyright: "Renovamen 2018-2022"
+      },
+      pwa: true,
+      search: false // use @vuepress/plugin-docsearch instead
+    },
+
+    footer: `
+      &copy; <a href="https://github.com/Renovamen" target="_blank">Renovamen</a> 2018-2022
+      <br>
+      Powered by <a href="https://v2.vuepress.vuejs.org" target="_blank">VuePress</a> &
+      <a href="https://github.com/Renovamen/vuepress-theme-gungnir" target="_blank">Gungnir</a>
+    `
+  }),
+
+  markdown: {
+    extractHeaders: {
+      level: [2, 3, 4, 5]
+    }
+  },
+
+  plugins: [
+    docsearchPlugin({
+      appId: "3DDLGP0IG4",
+      apiKey: "6556adaa82b31485309b440a525f264a",
+      indexName: "v2-vuepress-theme-gungnir",
+    })
+  ]
+});
